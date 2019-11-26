5a05be08b63ce74088eaa1bec8456b6b9230e9f1
vtxf <vtxf@qq.com>
2019-11-26 11:45:44 +0800

    list

--- Apps/Examples/scripts/list.js
+++ Apps/Examples/scripts/list.js
@@ -3,6 +3,14 @@ vueApp.examples = [
         "title": "Earth起步",
         "list": [
             {
+                "title": "创建地球",
+                "url": "./startup-createEarth.html"
+            },
+            {
+                "title": "使用Vue创建地球",
+                "url": "./startup-createEarthWithVue.html"
+            },
+            {
                 "title": "管子",
                 "url": "./startup-tube.html"
             }
