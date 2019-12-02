9abc737dcd814dececda5c3275f430fc9d9f4da7
vtxf <vtxf@qq.com>
2019-12-02 18:53:58 +0800

    add isCurve

--- src/Tube.js
+++ src/Tube.js
@@ -2,11 +2,155 @@
 // 演示如何创建扩展一个czmObject类型的对象
 
 // 0.1 纯粹直线管道构建
-// function createVertexForLines(rawPositions, radius, radialSegments) {
-    
-// }
 
-// 0.1 函数准备，借助THREE.js来获取管线的顶点坐标数值
+let scratchMatrix;
+let scratchCartesian;
+
+// 创建单段圆柱体
+function createSingleCylinder(pos0, pos1, radius, radialSegments, s0, s1, indexStart) {
+    let unitCylinderPositions;
+    let unitCylinderSts;
+    let unitCylinderIndices;
+
+    const r = XE.Obj.CustomPrimitive.Geometry.createCylinder(1.0, 1.0, 1.0, radialSegments);
+    unitCylinderPositions = r.positions;
+    unitCylinderSts = r.sts;
+    unitCylinderIndices = r.indices;
+
+    scratchMatrix = scratchMatrix || new Cesium.Matrix4();
+    const modelMatrix = getModelMatrix(pos0, pos1, radius, scratchMatrix);
+    
+    const positions = [];
+    scratchCartesian = scratchCartesian || new Cesium.Cartesian3();
+    const ucp = unitCylinderPositions;
+    const l = ucp.length / 3 | 0;
+    for (let i=0; i<l; ++i) {
+        const cartesian = Cesium.Cartesian3.fromElements(ucp[i*3+0], ucp[i*3+1], ucp[i*3+2]);
+        Cesium.Matrix4.multiplyByPoint(modelMatrix, cartesian, cartesian);
+        positions.push(cartesian.x, cartesian.y, cartesian.z);
+    }
+
+    const sts = [];
+    for (let i=0; i<l; ++i) {
+        const s = s0 + (s1-s0)*unitCylinderSts[i*2+1];
+        sts.push(s, unitCylinderSts[i*2+0]);
+    }
+
+    const indices = [];
+    const il = unitCylinderIndices.length;
+    for (let i=0; i<il; ++i) {
+        indices.push(unitCylinderIndices[i]+indexStart);
+    }
+
+    return {
+        positions,
+        sts,
+        indices,
+    };
+}
+
+let g_scratchCartesian;
+let g_scratchCartesian2;
+let g_scratchCartesian3;
+let g_scratchCartesian4;
+let g_scratchQuat;
+
+// 创建姿态矩阵，和单位圆柱体相乘后可以得到实际的圆柱体
+const getModelMatrix = (startPosition, endPosition, radius, result) => {
+    g_scratchCartesian = g_scratchCartesian || new Cesium.Cartesian3();
+    g_scratchCartesian2 = g_scratchCartesian2 || new Cesium.Cartesian3();
+    g_scratchCartesian3 = g_scratchCartesian3 || new Cesium.Cartesian3();
+    g_scratchCartesian4 = g_scratchCartesian4 || new Cesium.Cartesian3();
+    g_scratchQuat = g_scratchQuat || new Cesium.Quaternion();
+    
+    const scratchCartesian = g_scratchCartesian;
+    const scratchCartesian2 = g_scratchCartesian2;
+    const scratchCartesian3 = g_scratchCartesian3;
+    const scratchCartesian4 = g_scratchCartesian4;
+
+    const startCartesian = Cesium.Cartesian3.fromElements(startPosition[0], startPosition[1], startPosition[2], scratchCartesian);
+    const endCartesian = Cesium.Cartesian3.fromElements(endPosition[0], endPosition[1], endPosition[2], scratchCartesian2);
+    let dir = Cesium.Cartesian3.subtract(endCartesian, startCartesian, scratchCartesian3);
+    let depth = Cesium.Cartesian3.magnitude(dir);
+
+    if (depth > 0) {
+        depth = depth > 0 ? depth : 0.001;
+
+        dir = Cesium.Cartesian3.normalize(dir, dir);
+        const angle = Cesium.Cartesian3.angleBetween(dir, Cesium.Cartesian3.UNIT_Z);
+        const axis = Cesium.Cartesian3.cross(Cesium.Cartesian3.UNIT_Z, dir, scratchCartesian4);
+
+        const scale = Cesium.Cartesian3.fromElements(radius, radius, depth, scratchCartesian2);
+        const quat = Cesium.Quaternion.fromAxisAngle(axis, angle, g_scratchQuat);
+
+        result = result || new Cesium.Matrix4();
+        Cesium.Matrix4.fromTranslationQuaternionRotationScale(startCartesian, quat, scale, result);
+
+        return result;
+    } else {
+        return undefined;
+    }
+}
+
+// 创建多段圆柱体
+// 问题是连接处没有封闭
+function createVertexForLines(rawPositions, radius, radialSegments) {
+    const segments = rawPositions.length - 1;
+    if (segments <= 0) {
+        throw new Error('createVertexForLines error!')
+    }
+
+    const gpositions = [];
+    const rl = rawPositions.length;
+    for (let i = 0; i < rl; ++i) {
+        gpositions.push([rawPositions[i][0], rawPositions[i][1], rawPositions[i][2]]);
+    }
+
+    const [localPositions, centerL] = XE.Obj.CustomPrimitive.Geometry.getLocalPositions(gpositions);
+    let center = centerL;
+
+    const lp = localPositions;
+    const ll = (lp.length / 3 | 0) - 1;
+
+    let totalDistance = 0;
+    const distances = [0];
+    for (let i=0; i<ll; ++i) {
+        const dx = lp[(i+1)*3+0] - lp[i*3+0];
+        const dy = lp[(i+1)*3+1] - lp[i*3+1];
+        const dz = lp[(i+1)*3+2] - lp[i*3+2];
+        const distance = Math.sqrt(dx*dx+dy*dy+dz*dz);
+        distances.push(distance);
+        totalDistance += distance;
+    }
+
+    const positions = [];
+    const sts = [];
+    const indices = [];
+
+    let startIndex = 0;
+    for (let i=0; i<ll; ++i) {
+        const pos0 = [lp[i*3+0], lp[i*3+1], lp[i*3+2]];
+        const pos1 = [lp[(i+1)*3+0], lp[(i+1)*3+1], lp[(i+1)*3+2]];
+        const s0 = distances[i] / totalDistance;
+        const s1 = distances[i+1] / totalDistance;
+        const indexStart = startIndex;
+        const { positions: lps, sts: lsts, indices: lindices } = createSingleCylinder(pos0, pos1, radius, radialSegments, s0, s1, indexStart);
+        startIndex += (lps.length / 3 | 0);
+
+        lps.forEach(e => positions.push(e));
+        lsts.forEach(e => sts.push(e));
+        lindices.forEach(e => indices.push(e));
+    }
+
+    return {
+        center,
+        positions,
+        sts,
+        indices,
+    };
+}
+
+// 0.2 函数准备，借助THREE.js来获取管线的顶点坐标数值
 function createVertexForTube(rawPositions, tubularSegments, radius, radialSegments, closed) {
     if (!rawPositions || rawPositions.length <= 1) {
         throw new Error('rawPositions error!')
@@ -158,6 +302,14 @@ const defaultOptions = {
     * @memberof Tube
     */
     color: [1, 1, 1, 1],
+    /**
+    * 是否为曲线，如果为false，则由一段段的直线构成，直线连接处没有过渡体
+    * @type {boolean}
+    * @instance
+    * @default false
+    * @memberof Tube
+    */        
+    isCurve: true,
 };
 
 // 1 基于XE.Core.XbsjCzmObj创建一个自定义的类
@@ -174,19 +326,34 @@ class Tube extends XE.Core.XbsjCzmObj {
         // XE.MVVM.watch的返回值是一个函数，调用该函数可以取消监控。把该函数放入disposer数组以后，它就会在对象销毁时自动执行。
         const update = () => {
             if (this.positions.length > 1) {
-                const {
-                    center,
-                    positions,
-                    sts,
-                    normals,
-                    indices,
-                } = createVertexForTube(this.positions, this.tubularSegments, this.radius, this.radialSegments, this.closed);
+                if (this.isCurve) {
+                    const {
+                        center,
+                        positions,
+                        sts,
+                        normals,
+                        indices,
+                    } = createVertexForTube(this.positions, this.tubularSegments, this.radius, this.radialSegments, this.closed);
+        
+                    this._customPrimitive.position = center;
+                    this._customPrimitive.positions = positions;
+                    this._customPrimitive.sts = sts;
+                    this._customPrimitive.normals = normals;
+                    this._customPrimitive.indices = indices;
+                } else {
+                    const {
+                        center,
+                        positions,
+                        sts,
+                        indices,
+                    } = createVertexForLines(this.positions, this.radius, this.radialSegments);
 
-                this._customPrimitive.position = center;
-                this._customPrimitive.positions = positions;
-                this._customPrimitive.sts = sts;
-                this._customPrimitive.normals = normals;
-                this._customPrimitive.indices = indices;
+                    this._customPrimitive.position = center;
+                    this._customPrimitive.positions = positions;
+                    this._customPrimitive.sts = sts;
+                    this._customPrimitive.normals = undefined;
+                    this._customPrimitive.indices = indices;
+                }
             }
         }
 
@@ -197,6 +364,7 @@ class Tube extends XE.Core.XbsjCzmObj {
             radius: this.radius,
             radialSegments: this.radialSegments,
             closed: this.closed,
+            isCurve: this.isCurve,
         }), update));
 
         const update2 = () => {
